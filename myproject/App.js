c46c89626ce45906644332d1eef6089342b8c02b
Bogdan <bogdan.pechounov@gmail.com>
2023-05-02 12:57:29 -0400

    todo app

--- myproject/App.js
+++ myproject/App.js
@@ -1,13 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react'
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native'
 
 export default function App() {
+  const [name, setName] = useState('User')
+
+  const clickHandler = () => {
+    setName('Test')
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Text style={styles.boldText}>Hello {name}!</Text>
+      </View>
+      <View style={styles.body}>
+        <Text>Enim ut esse esse proident cupidatat cupidatat ut.</Text>
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button title='update state' onPress={clickHandler} />
+      </View>
+      <TextInput
+        style={styles.input}
+        placeholder='Name'
+        onChangeText={(v) => setName(v)}
+      />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +35,25 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  header: {
+    backgroundColor: 'pink',
+    padding: 20,
+  },
+  boldText: {
+    fontWeight: 'bold',
+  },
+  body: {
+    backgroundColor: 'yellow',
+    padding: 20,
+  },
+  buttonContainer: {
+    marginTop: 20,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#777',
+    padding: 8,
+    margin: 10,
+    width: 200,
+  },
+})
---
+++ todo-app/App.js
@@ -0,0 +1,69 @@
+import { StatusBar } from 'expo-status-bar'
+import { useState } from 'react'
+import { FlatList } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
+import Header from './components/Header'
+import TodoItem from './components/TodoItem'
+import AddTodo from './components/AddTodo'
+import { Alert } from 'react-native'
+import { TouchableWithoutFeedback } from 'react-native'
+import { Keyboard } from 'react-native'
+
+export default function App() {
+  const [todos, setTodos] = useState([
+    { text: 'buy coffee', key: '1' },
+    { text: 'create an app', key: '2' },
+    { text: 'play on the switch', key: '3' },
+  ])
+
+  const pressHandler = (key) => {
+    setTodos((todos) => todos.filter((todo) => todo.key !== key))
+  }
+
+  const submitHandler = (text) => {
+    if (text.length > 3) {
+      setTodos((todos) => [...todos, { text, key: Math.random().toString() }])
+    } else {
+      Alert.alert('Oups!', 'Todo must be over 3 characters long', [
+        { text: 'Understood', onPress: () => console.log('alert closed') },
+      ])
+    }
+  }
+
+  return (
+    <TouchableWithoutFeedback
+      onPress={() => {
+        // dismiss keyboard
+        Keyboard.dismiss()
+      }}
+    >
+      <View style={styles.container}>
+        <Header />
+        <View style={styles.content}>
+          <AddTodo submitHandler={submitHandler} />
+          <View style={styles.list}>
+            <FlatList
+              data={todos}
+              renderItem={({ item }) => (
+                <TodoItem item={item} pressHandler={pressHandler} />
+              )}
+            />
+          </View>
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  content: {
+    padding: 40,
+  },
+  list: {
+    marginTop: 20,
+  },
+})
